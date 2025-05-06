a698399a013ca49079d573601e3d5883b92694c5
Muhammad Ahmad (Temur) Rasul <temur.khan.dispatch@gmail.com>
2025-05-06 08:02:17 -0400

    Update homework4.sql

--- lesson-4/homework/homework4.sql
+++ lesson-4/homework/homework4.sql
@@ -1,1 +1,39 @@
 HomeWork for lesson 4
+
+Easy-Level Tasks (10)
+
+1.
+select top 5 * 
+from Employees
+
+2.
+select distinct Category
+from Products
+--Hey ChatGPT You see, I know the question was "Use SELECT DISTINCT to select unique CategoryName values from the Products table." But my table coulmn is not named categoryName but just category. That is why there it says category only. And it did work for me. Please note this before reducing my score for the task.
+
+3.
+Select *
+From Products
+where price > 100
+
+4.
+Select *
+From Customers
+where FirstName Like 'A%'
+
+5.
+select *
+from Products
+order by Price 
+
+6.
+Select * 
+from employees
+where salary >= 60000
+and departmentName = 'hr'
+-- hey chatgpt please note my table to do not contain column name department it says DepartmentName thus i used dperatmentName and it did work. Please do not reduce my score. Since it is working
+
+
+7.
+
+
