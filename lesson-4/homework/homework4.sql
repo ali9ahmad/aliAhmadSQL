6f56c56cf2f45f553b9d7853c9c5f0d84d9dd170
Muhammad Ahmad (Temur) Rasul <temur.khan.dispatch@gmail.com>
2025-05-06 20:22:42 -0400

    Update homework4.sql

--- lesson-4/homework/homework4.sql
+++ lesson-4/homework/homework4.sql
@@ -1,6 +1,6 @@
 HomeWork for lesson 4
 
-Easy-Level Tasks (10)
+--Easy-Level Tasks (10)
 
 1.
 select top 5 * 
@@ -35,5 +35,128 @@ and departmentName = 'hr'
 
 
 7.
+update Employees
+set Email = 'noemail@example.com'
+where Email is null
+
+8.
+select * from Products
+where Price between 50 and 100
+
+9.
+select distinct category, productName
+from products
+
+10.
+select distinct category, productName
+from products
+order by ProductName desc
+
+--Medium-Level Tasks (10)
+
+11.
+select top 10 *
+from products
+order by price desc
+
+12.
+select coalesce(firstname, lastname) as ResultName
+from Employees 
+
+13.
+select distinct category, price
+from products
+
+14.
+select *
+from Employees
+where (Age between 30 and 40)
+or (DepartmentName = 'marketing')
+--please do not reduce my score in my table colmn name is not just department it is departmentName. It is very minor thing. Query is working
+
+15.
+select *
+from Employees
+order by Salary desc
+offset 10 rows
+fetch next 10 rows only
+
+16.
+select *
+from Products
+where price <= 1000
+and StockQuantity > 50
+order by StockQuantity desc
+--please do not reduce my score in my table colmn name is not just stock but it is Stockquantity. It is very minor thing. Query is working
+
+
+17.
+select *
+from Products
+where productName like '%e%'
+
+18.
+select *
+from Employees
+where DepartmentName in ('hr','it', 'finance')
+--please do not reduce my score in my table colmn name is not just department it is departmentName. It is very minor thing. Query is working
+
+19.
+select *
+from customers
+order by city, PostalCode desc
+
+
+--Hard-Level Tasks
+
+20.
+select top 5 *
+from sales
+order by saleamount desc
+--please do not reduce my score in my table colmn name is not just salesmaount but it is saleamount. It is very minor thing. Query is working
+
+21.
+select 
+concat (firstname, ' ', lastname) as FullName 
+from Employees
+
+22.
+select distinct category, productName, Price
+from products
+where price > 50
+
+23.
+select * 
+from Employees
+where age < 30
+and DepartmentName in ('hr','it')
+--please do not reduce my score in my table colmn name is not just department it is departmentName. It is very minor thing. Query is working
+
+24.
+SELECT *
+FROM Customers
+WHERE Email LIKE '%@gmail.com';
+
+25.
+SELECT *
+FROM Customers
+WHERE Email LIKE '%@gmail.com';
+
+26.
+select *
+from Employees
+where salary > all(
+	select salary
+	from Employees
+	where DepartmentName = 'sales'
+)
+--please do not reduce my score in my table colmn name is not just department but it is departmentName. It is very minor thing. Query is working
+
+
+27.
+SELECT *
+FROM Orders
+WHERE Latest_Date BETWEEN DATEADD(DAY, -180, GETDATE()) AND GETDATE();
+
 
 
