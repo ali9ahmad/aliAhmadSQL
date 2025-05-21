1f7f434dfb2785e00bf3c18ca9acc670ef8fd11f
Muhammad Ahmad (Temur) Rasul <temur.khan.dispatch@gmail.com>
2025-05-21 01:10:49 -0400

    Update homework9.sql

--- lesson-9/homework/homework9.sql
+++ lesson-9/homework/homework9.sql
@@ -78,6 +78,65 @@ from suppliers s
 join products p on s.supplierid = p.supplierid
 where suppliername like '%tech%'
 
+16.
+select o.orderid, p.amount, o.totalamount
+from orders o 
+join payments p on p.orderid = o.orderid
+where p.amount < o.totalamount
 
+17.
+select d.departmentName, e.name
+from employees e
+join departments d on d.departmentid = e.departmentid
+
+18.
+select p.productName, c.categoryname
+from products p
+join categories c on c.categoryid = p.categoryid
+where c.categoryname in ('electronics','furniture')
+
+19.
+SELECT s.SaleID, s.ProductID, s.SaleAmount, c.Country
+FROM Sales s
+INNER JOIN Customers c ON s.CustomerID = c.CustomerID
+WHERE c.Country = 'USA';
+
+20.
+SELECT o.OrderID, o.TotalAmount
+FROM Orders o
+INNER JOIN Customers c ON o.CustomerID = c.CustomerID
+WHERE c.Country = 'Germany' AND o.TotalAmount > 100;
+
+21.
+SELECT e1.Name AS Employee1, e2.Name AS Employee2
+FROM Employees e1
+JOIN Employees e2 ON e1.DepartmentID <> e2.DepartmentID
+WHERE e1.EmployeeID < e2.EmployeeID;
+
+22.
+SELECT p.PaymentID, p.Amount, o.Quantity, pr.Price
+FROM Payments p
+INNER JOIN Orders o ON p.OrderID = o.OrderID
+INNER JOIN Products pr ON o.ProductID = pr.ProductID
+WHERE p.Amount <> (o.Quantity * pr.Price);
+
+23.
+SELECT s.StudentID, s.Name
+FROM Students s
+LEFT JOIN Enrollments e ON s.StudentID = e.StudentID
+WHERE e.CourseID IS NULL;
+
+24.
+SELECT e1.Name AS Manager, e1.Salary AS ManagerSalary, e2.Name AS ManagedEmployee, e2.Salary AS ManagedSalary
+FROM Employees e1
+JOIN Employees e2 ON e1.EmployeeID = e2.ManagerID
+WHERE e1.Salary <= e2.Salary;
+
+25.
+SELECT c.CustomerID, c.FirstName
+FROM Customers c
+JOIN Orders o ON c.CustomerID = o.CustomerID
+LEFT JOIN Payments p ON o.OrderID = p.OrderID
+WHERE p.PaymentID IS NULL;
 
 
